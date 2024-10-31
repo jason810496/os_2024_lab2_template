25d012e8881251834d4547b2fb5471389dee2021
LIU ZHE YOU <68415893+jason810496@users.noreply.github.com>
2024-10-31 19:47:56 +0800

    fix `pipe_num` calculation

--- src/command.c
+++ src/command.c
@@ -68,6 +68,7 @@ struct cmd *split_line(char *line)
 			new_pipe->next = NULL;
 			temp->next = new_pipe;
 			temp = new_pipe;
+			new_cmd->pipe_num++;
         } else if (token[0] == '<') {
 			token = strtok(NULL, " ");
             temp->in_file = token;
@@ -79,7 +80,7 @@ struct cmd *split_line(char *line)
 			temp->length++;
         }
         token = strtok(NULL, " ");
-		new_cmd->pipe_num++;
+		
 
     }
 
